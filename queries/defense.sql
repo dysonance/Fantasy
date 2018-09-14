44f888863afb9b9ec8222b9982bf5cdfb763dfb3
Jacob Amos <jamos125@gmail.com>
2018-09-14 17:17:17 -0400

    julia defense t-stat calc work replicated in sql

--- queries/defense.sql
+++ queries/defense.sql
@@ -8,7 +8,7 @@ with drive_team as (
         case when g.home_team = d.pos_team then g.away_team else g.home_team end defense_team,
         case when g.home_team = d.pos_team then g.home_team else g.away_team end offense_team,
         g.home_team,
-        g.away_team,
+        g.away_team
     from
         drive d,
         game g
@@ -20,6 +20,7 @@ with drive_team as (
         week,
         drive_id
 ),
+
 team_plays as (
     select
         dt.year,
@@ -42,5 +43,113 @@ team_plays as (
     where
         dt.gsis_id = ag.gsis_id
         and dt.drive_id = ag.drive_id
+),
+
+passavg as (
+    select
+        year,
+        week,
+        avg(pass_yards)
+    from
+        team_plays
+    where
+        passes != 0
+    group by
+        year,
+        week
+),
+
+rushavg as (
+    select
+        year,
+        week,
+        avg(rush_yards)
+    from
+        team_plays
+    where
+        rushes != 0
+    group by
+        year,
+        week
+),
+
+passing as (
+    select
+        tp.year,
+        tp.week,
+        tp.team,
+        count(tp.play_count) as plays,
+        avg(tp.pass_yards) as team_avg,
+        stddev(tp.pass_yards) as team_dev,
+        avg(pa.avg) as league_avg
+    from
+        team_plays tp
+        left join passavg pa on tp.year=pa.year and tp.week=pa.week
+    where
+        tp.passes != 0
+        and tp.year = pa.year
+        and tp.week = pa.week
+    group by
+        tp.year,
+        tp.week,
+        tp.team
+),
+
+rushing as (
+    select
+        tp.year,
+        tp.week,
+        tp.team,
+        count(tp.play_count) as plays,
+        avg(tp.rush_yards) as team_avg,
+        stddev(tp.rush_yards) as team_dev,
+        avg(ra.avg) as league_avg
+    from
+        team_plays tp
+        left join rushavg ra on tp.year=ra.year and tp.week=ra.week
+    where
+        rushes != 0
+    group by
+        tp.year,
+        tp.week,
+        tp.team
+),
+
+defense as (
+    select
+        p.year,
+        p.week,
+        p.team,
+        p.plays as npp,
+        r.plays as nrp,
+        p.team_avg as pyta,
+        r.team_avg as ryta,
+        --  p.team_dev as pytd,
+        --  r.team_dev as rytd,
+        p.league_avg as pyla,
+        r.league_avg as ryla,
+        (sqrt(p.plays) * (p.team_avg - p.league_avg) / p.team_dev) as pt,
+        (sqrt(r.plays) * (r.team_avg - r.league_avg) / r.team_dev) as rt
+    from
+        passing p,
+        rushing r
+    where
+        p.year = r.year
+        and p.week = r.week
+        and p.team = r.team
 )
-select * from team_plays
+
+select
+    d.year,
+    d.week,
+    d.team,
+    d.pt,
+    d.rt,
+    ((d.pt + d.rt) * -1) as score
+from
+    defense d
+
+order by
+    year desc,
+    week desc,
+    score desc
