074029f03a497ff853271b704fff92b036de35be
Jacob Amos <jamos125@gmail.com>
2018-09-19 16:05:07 -0400

    add share of carries and targets to runningback stats

--- queries/runningbacks.sql
+++ queries/runningbacks.sql
@@ -1,4 +1,35 @@
-with rb_plays as (
+with play_stats as (
+    select
+        g.gsis_id,
+        g.week,
+        g.season_year as year,
+        case when g.home_team = d.pos_team then g.away_team else g.home_team end defense_team,
+        case when g.home_team = d.pos_team then g.home_team else g.away_team end offense_team,
+        sum(case when ag.passing_att>0 or ag.rushing_att>0 then 1 else 0 end) as plays,
+        sum(case when ag.passing_att>0 then 1 else 0 end) as passes,
+        sum(case when ag.rushing_att>0 then 1 else 0 end) as rushes
+    from
+        game g,
+        drive d,
+        agg_play ag
+    where
+        g.gsis_id = d.gsis_id
+        and g.gsis_id = ag.gsis_id
+        and d.drive_id = ag.drive_id
+        and g.season_type = 'Regular'
+    group by
+        g.gsis_id,
+        g.season_year,
+        g.week,
+        offense_team,
+        defense_team
+    order by
+        year desc,
+        week desc,
+        offense_team
+),
+
+rb_plays as (
     select
         g.gsis_id,
         g.season_year as year,
@@ -49,13 +80,19 @@ rb_stats as (
 )
 
 select
-    rb.*
+    rb.*,
+    round((rb.carries)::numeric / ps.rushes, 2) as cpr,
+    round((rb.targets)::numeric / ps.passes, 2) as tpp
 from
-    rb_stats rb
+    rb_stats rb,
+    play_stats ps
+where
+    ps.year = rb.year
+    and ps.week = rb.week
+    and ps.offense_team = rb.team
 order by
     rb.year desc,
     rb.week desc,
     rb.carries desc,
     rb.targets desc,
     rb.team
---  limit 50
