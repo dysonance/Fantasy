7e68dead8e1cc3f8e218a80e208e9a1c3d28f52a
Jacob Amos <jamos125@gmail.com>
2018-09-26 18:19:00 -0400

    functiona added to visualize team defense by years and weeks

--- src/defense.r
+++ src/defense.r
@@ -14,3 +14,25 @@ if (!exists("CONNECTION")){
 
 query = paste(readLines("queries/defense.sql"), collapse=' ')
 defense = RunQuery(CONNECTION, query)
+defense[, dt:=pt+rt]
+
+PlotDefenseRankings = function(defense, yr=year(Sys.Date()), wk=NULL)
+{
+  if (is.null(yr))
+  {
+    yr = sort(unique(defense[,year]))
+  }
+  if (is.null(wk))
+  {
+    wk = sort(unique(defense[year %in% yr, week]))
+  }
+  X = defense[year %in% yr & week %in% wk,
+              .(dt=mean(dt), pt=mean(pt), rt=mean(rt), nrp=sum(nrp), npp=sum(npp)),
+              by=team]
+  par(mfrow=c(2,1), family='mono', cex=0.75)
+  barplot(X[order(pt),pt], names.arg=X[order(pt),team], col='#800080', main='Passing Defense')
+  grid(lty=1, col='#00000020', nx=0, ny=NULL)
+  barplot(X[order(rt),rt], names.arg=X[order(rt),team], col='#800000', main='Rushing Defense')
+  grid(lty=1, col='#00000020', nx=0, ny=NULL)
+}
+PlotDefenseRankings(defense, 2018, NULL)
