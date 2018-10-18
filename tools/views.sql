16a08874d594e69c6a9f3574daf452402670a757
Jacob Amos <jamos125@gmail.com>
2018-10-18 11:43:31 -0400

    materalized view for team balance added

--- tools/views.sql
+++ tools/views.sql
@@ -48,3 +48,56 @@ create materialized view fantasy_points as (
     order by
         points desc
 );
+
+drop materialized view if exists team_balance;
+create materialized view team_balance as (
+    with team_plays as (
+        select
+            g.gsis_id,
+            ag.drive_id,
+            ag.play_id,
+            g.season_year as year,
+            g.week,
+            offense_team(ag.gsis_id, ag.drive_id) as offense,
+            defense_team(ag.gsis_id, ag.drive_id) as defense,
+            (ag.passing_att > 0 or ag.passing_sk_yds < 0)::integer as pass_play,
+            (ag.rushing_att > 0)::integer as run_play
+        from
+            agg_play ag,
+            game g
+        where
+            g.season_type = 'Regular'
+            and ag.gsis_id = g.gsis_id
+            and (ag.passing_att > 0 or ag.passing_sk_yds < 0 or ag.rushing_att > 0)
+        order by
+            g.season_year desc,
+            g.week desc,
+            offense,
+            ag.drive_id,
+            ag.play_id
+    )
+    select
+        tp.year,
+        tp.week,
+        tp.offense,
+        tp.defense,
+        count(tp.play_id) as n_plays,
+        sum(tp.pass_play) as n_pass,
+        sum(tp.run_play) as n_run,
+        sum(tp.pass_play)/count(tp.play_id)::float as pct_pass,
+        sum(tp.run_play)/count(tp.play_id)::float as pct_run
+    from
+        team_plays tp
+    group by
+        tp.year,
+        tp.week,
+        tp.offense,
+        tp.defense
+    order by
+        tp.year desc,
+        tp.week desc,
+        tp.offense
+);
+
+alter materialized view fantasy_points owner to nfldb;
+alter materialized view team_balance owner to nfldb;
