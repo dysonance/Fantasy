02dd3e7609d6a46a3c2c0954acf6c423d669e516
Jacob Amos <jamos125@gmail.com>
2018-10-18 17:33:19 -0400

    code cleanup and updated scoring algorithm

--- src/receivers.r
+++ src/receivers.r
@@ -1,7 +1,9 @@
 source("tools/connect.r")
 
+YEAR = 2018
+MIN_REL_TGT_PCT = 0.75
+
 fp = RunQuery(CONNECTION, "select * from fantasy_points")
-FP = fp[, .(avg_pts=mean(points)), by=.(position,year)][order(year,position)]
 
 tb = RunQuery(CONNECTION, "select * from team_balance")
 TB = tb[,
@@ -32,11 +34,10 @@ TE = te[,
          yds=sum(yds),
          vol=sd(yds),
          relyds=mean(yds/togo,na.rm=T)),
-       by=.(year,team,name)][year==2018]
-TE = merge(TE, TB[,.(offense,year,pct_pass)], by.x=c('team','year'), by.y=c('offense','year'))
-TE[,score:=targets*tgt_pct*pct_pass*catchrate*yds/catches*relyds/vol]
-TE = TE[order(score)][score>1]
-TE[,score:=log(score)*FP[position=='TE' & year==2018, avg_pts]]
+       by=.(year,team,name)][year==YEAR]
+TE = merge(TE, TB[,.(offense,year,pct_pass,n_plays)], by.x=c('team','year'), by.y=c('offense','year'))
+TE[,score:=log(targets*pct_pass*catchrate*yds/catches*relyds/vol)*fp[year==YEAR&position=='TE',mean(points)]]
+TE = TE[order(score)][tgt_pct>=quantile(tgt_pct,MIN_REL_TGT_PCT)[[1]]&catches>1]
 TE
 
 
@@ -50,30 +51,28 @@ WR = wr[,
          yds=sum(yds),
          vol=sd(yds),
          relyds=mean(yds/togo,na.rm=T)),
-       by=.(year,team,name)][year==2018]
-WR = merge(WR, TB[,.(offense,year,pct_pass)], by.x=c('team','year'), by.y=c('offense','year'))
-WR[,score:=targets*tgt_pct*pct_pass*catchrate*yds/catches*relyds/vol]
-WR = WR[order(score)][score>1&is.finite(score)]
-WR[,score:=log(score)*FP[position=='WR' & year==2018, avg_pts]]
+       by=.(year,team,name)][year==YEAR]
+WR = merge(WR, TB[,.(offense,year,pct_pass,n_plays)], by.x=c('team','year'), by.y=c('offense','year'))
+WR[,score:=log(targets*pct_pass*tgt_pct*catchrate*yds/catches*relyds/vol)*fp[year==YEAR&position=='WR',mean(points)]]
+WR = WR[order(score)][tgt_pct>=quantile(tgt_pct,MIN_REL_TGT_PCT)[[1]]&catches>1]
 WR
 
 PlotCatcherValues = function(WR, TE, outfile="figures/wr-te.png"){
   if (!is.null(outfile)){
     png(outfile, width=11, height=8.5, units='in')
   }
-  par(mfrow=c(1,2), cex=0.5, family='mono', mar=c(2.5,2.5,2.5,2.5), mgp=c(1.5,0.5,0))
+  par(mfrow=c(1,2), cex=0.5, family='mono', mar=c(3,3,3,3), mgp=c(1.5,0.5,0))
   # tight end value rankings
-  b = barplot(TE[,score], horiz=T)
-  text(x=TE[,score], y=b[,1], labels=TE[,name])
+  b = barplot(TE[,score], horiz=T, col='#00FFFF80', border='#008080')
+  text(x=TE[,score], y=b[,1], labels=TE[,name], adj=c(1,0.5))
   box(bty='l')
   grid(lty=1, nx=NULL, ny=0, col='#00000020')
-  title(main="Tight End Rankings")
+  title(main="Tight End Rankings", xlab="Value")
   # wide receiver value rankings
-  b = barplot(WR[,score], horiz=T)
-  text(x=WR[,score], y=b[,1], labels=WR[,name])
+  b = barplot(WR[,score], horiz=T, col='#FF000080', border='#800000')
+  text(x=WR[,score], y=b[,1], labels=WR[,name], adj=c(1,0.5))
   box(bty='l')
   grid(lty=1, nx=NULL, ny=0, col='#00000020')
-  title(main="Wide Receiver Rankings")
+  title(main="Wide Receiver Rankings", xlab="Value")
 }
-
 PlotCatcherValues(WR, TE, NULL)
