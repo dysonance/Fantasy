1f7fc71bca94c814adf4141b0154beff51f57c02
Jacob Amos <jamos125@gmail.com>
2018-11-20 15:35:55 -0500

    drastically simplified julia querying

--- src/query.jl
+++ src/query.jl
@@ -4,12 +4,13 @@ using LibPQ, DataStreams, DataFrames
 
 function run_query(connection, query)
     result = execute(connection, query)
-    dataframe_stream = Data.stream!(result, DataFrame)
-    data = DataFrame()
-    for (j, field) in enumerate(dataframe_stream.header)
-        data[Symbol(field)] = dataframe_stream.columns[j]
-    end
-    return data
+    return fetch!(DataFrame, result)
+    # dataframe_stream = Data.stream!(result, DataFrame)
+    # data = DataFrame()
+    # for (j, field) in enumerate(dataframe_stream.header)
+    #     data[Symbol(field)] = dataframe_stream.columns[j]
+    # end
+    # return data
 end
 
 # teams = run_query(connection, "select * from team;")
