1599a490312cf47950ae5b3959a18f9da0c3e831
Jacob Amos <jamos125@gmail.com>
2018-12-06 16:19:06 -0500

    clean up julia query code file. add utility function for connecting.

--- src/query.jl
+++ src/query.jl
@@ -1,18 +1,8 @@
 using LibPQ, DataStreams, DataFrames
 
-# connection = LibPQ.Connection("dbname=nfldb")
+connect() = LibPQ.Connection("dbname=nfldb")
 
-function run_query(connection, query)
+function query(connection, query)
     result = execute(connection, query)
     return fetch!(DataFrame, result)
-    # dataframe_stream = Data.stream!(result, DataFrame)
-    # data = DataFrame()
-    # for (j, field) in enumerate(dataframe_stream.header)
-    #     data[Symbol(field)] = dataframe_stream.columns[j]
-    # end
-    # return data
 end
-
-# teams = run_query(connection, "select * from team;")
-
-# close(connection)
