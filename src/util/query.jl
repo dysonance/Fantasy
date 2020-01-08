c68f9831f84980336518ea7d0c090763c3c9589c
Jacob Amos <jamos125@gmail.com>
2020-01-08 04:04:54 -0500

    more flexible jl db queries

--- src/util/query.jl
+++ src/util/query.jl
@@ -2,6 +2,12 @@ using LibPQ, DataStreams, DataFrames
 
 connect() = LibPQ.Connection("dbname=nfldb")
 
-function query(connection, query)
-    return DataFrame(execute(connection, query))
+function query(connection::LibPQ.Connection, qry::String=""; file::String="")::DataFrame
+    result = DataFrame()
+    if length(qry) > 0
+        result = DataFrame(execute(connection, qry))
+    elseif length(file) > 0 && isfile(file)
+        result = DataFrame(execute(connection, String(read(file))))
+    end
+    return result
 end
